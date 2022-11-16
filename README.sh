bb66db75ec2bbb02c8d014acf9ee85953dff73c5
Tor Colvin <tor.colvin@couchbase.com>
2022-11-16 16:40:43 -0500

    Update README.sh

--- README.sh
+++ README.sh
@@ -1,5 +1,3 @@
 This will kill all docker containers on your local network, but I test with:
 
-```
 docker kill $(docker ps -qa) ; docker rm $(docker ps -qa) ; docker network rm workshop ; ./run.sh && docker logs -f sync-gateway
-```
